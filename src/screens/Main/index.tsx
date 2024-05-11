a9b00ebace22798abe80921945d1df716498633e
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-11 15:29:19 +0300

    chore: create empty screens

---
+++ src/screens/Main/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export const Main = () => {
+  return (
+    <View>
+      <Text>Main</Text>
+    </View>
+  );
+};
---
+++ src/screens/Onboarding/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export const Onboarding = () => {
+  return (
+    <View>
+      <Text>Onboarding</Text>
+    </View>
+  );
+};
