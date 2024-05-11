667f4e4fed4abfe3a05bae382ba2c8d81204120a
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-11 22:02:19 +0300

    feat: add background color to status bar

--- App.tsx
+++ App.tsx
@@ -2,10 +2,13 @@ import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { MainNavigation } from '@/navigation/Main';
+import { StatusBar } from 'react-native';
+import { colors } from '@/constants/colors';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={colors.primary} />
       <MainNavigation />
     </NavigationContainer>
   );
