dfd4a96e8f2abde9554ff6177413708a93201136
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-11 16:13:01 +0300

    feat: create reusable Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import { Pressable } from 'react-native';
+import { styles } from './styles';
+import { ButtonProps } from './types';
+
+export const Button = ({ children, ...props }: ButtonProps) => {
+  return (
+    <Pressable {...props} style={({ pressed }) => [styles.btn, pressed && styles.pressedBtn]}>
+      {children}
+    </Pressable>
+  );
+};
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import { colors } from '@/constants/colors';
+import { spacing } from '@/constants/spacing';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  btn: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.primary,
+    borderRadius: spacing.sm,
+    paddingVertical: spacing.sm,
+  },
+  pressedBtn: {
+    opacity: 0.7,
+  },
+});
