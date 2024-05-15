97a6683f013f76549d86feba19f7e2cda55effb6
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-15 12:30:22 +0300

    chore: add eslint sort plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['simple-import-sort'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
   },
 };
