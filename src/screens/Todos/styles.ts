0326debbe7229d297365270cbb1f889e267c1af2
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-15 12:22:41 +0300

    feat: create TodoList screen

---
+++ src/screens/Todos/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  todosContainer: {
+    flex: 0.8,
+    paddingBottom: 32,
+  },
+  doneTodosContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+
+  addTodosContainer: {
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+});
