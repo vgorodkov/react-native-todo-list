772447ad4f59eb7137e8c42326aa2b265f67fca9
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-15 12:19:56 +0300

    feat: create header that on press expands DoneTaskList

---
+++ src/components/DoneTaskListHeader/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  doneTaskListHeader: {
+    backgroundColor: 'white',
+    paddingTop: 8,
+    height: 40,
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 16,
+  },
+  arrowDownIcon: { width: 24, height: 24 },
+});
