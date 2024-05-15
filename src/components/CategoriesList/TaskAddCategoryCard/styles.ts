fc4ace80a8101e6c77ce161aa5b01b745c1f1240
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-15 22:11:41 +0300

    feat: create CategoriesList

---
+++ src/components/CategoriesList/TaskAddCategoryCard/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+import { spacing } from '@/constants/spacing';
+
+export const styles = StyleSheet.create({
+  addTaskCategory: {
+    height: 104,
+    width: 104,
+    margin: spacing.s,
+    borderRadius: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#D25EB0',
+  },
+  addTaskCategoryImg: {
+    width: 60,
+    height: 60,
+  },
+});
