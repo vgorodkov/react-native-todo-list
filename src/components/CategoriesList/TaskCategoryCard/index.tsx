cd5ff235d5598808106ca2566d9c3b4014605fd1
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-15 22:11:29 +0300

    feat: create TaskCategoryComponent

---
+++ src/components/CategoriesList/TaskCategoryCard/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Image, View } from 'react-native';
+
+import { Text } from '@/components/UI';
+import { TextVariant } from '@/components/UI/Text/types';
+import { colors } from '@/constants/colors';
+
+import { styles } from './styles';
+import { TaskCategoryProps } from './types';
+
+export const TaskCategoryCard = ({ title, img, backgroundColor }: TaskCategoryProps) => {
+  return (
+    <View style={[styles.taskCategory, { backgroundColor }]}>
+      <Text
+        variant={TextVariant.label_large}
+        style={styles.taskAmountBadge}
+        color={colors.onPrimary}
+      >
+        {1}
+      </Text>
+      <Image style={styles.taskCategoryImg} source={img} />
+      <Text style={styles.taskText} variant={TextVariant.label_large} color="white">
+        {title}
+      </Text>
+    </View>
+  );
+};
