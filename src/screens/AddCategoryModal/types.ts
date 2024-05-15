69c7bdc0149b9554272aa2aee64eaf690deeaccf
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-15 22:12:13 +0300

    feat: create AddCategoryModal

---
+++ src/screens/AddCategoryModal/types.ts
@@ -0,0 +1,8 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import { MainStackParamList } from '@/types/navigation';
+
+export type AddCategoryModalScreenProps = NativeStackScreenProps<
+  MainStackParamList,
+  'AddCategoryModal'
+>;
