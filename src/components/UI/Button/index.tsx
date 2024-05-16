b2d5a7764c143ac491f9e81b6bde7922fb6726c2
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-16 10:25:48 +0300

    feat: add disabled style and text button style

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -1,12 +1,30 @@
 import { Pressable } from 'react-native';
 
+import { colors } from '@/constants/colors';
+
+import { Text } from '../Text';
+import { TextVariant } from '../Text/types';
 import { styles } from './styles';
 import { ButtonProps } from './types';
 
-export const Button = ({ children, ...props }: ButtonProps) => {
+export const Button = ({ children, variant = 'solid', ...props }: ButtonProps) => {
+  const { disabled } = props;
   return (
-    <Pressable {...props} style={({ pressed }) => [styles.btn, pressed && styles.pressedBtn]}>
-      {children}
+    <Pressable
+      {...props}
+      style={({ pressed }) => [
+        styles.btn,
+        variant === 'solid' ? styles.solidBtn : styles.textBtn,
+        pressed && styles.pressedBtn,
+        disabled && variant === 'solid' && styles.disabledBtn,
+      ]}
+    >
+      <Text
+        variant={TextVariant.label_large}
+        color={disabled ? '#727272' : variant === 'solid' ? colors.onPrimary : colors.primary}
+      >
+        {children}
+      </Text>
     </Pressable>
   );
 };
