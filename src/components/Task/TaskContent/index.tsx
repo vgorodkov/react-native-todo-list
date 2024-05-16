f35267fb3cffd018c4c60236823ea76e0f12654c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-16 10:21:17 +0300

    feat: add toggle checkbox functionality to Task

--- src/components/Task/TaskContent/index.tsx
+++ src/components/Task/TaskContent/index.tsx
@@ -5,15 +5,21 @@ import { Text } from '@/components/UI';
 import { Checkbox } from '@/components/UI/CheckBox';
 import { TextVariant } from '@/components/UI/Text/types';
 import { colors } from '@/constants/colors';
+import { useAppDispatch } from '@/store/hooks';
+import { toggleTaskIsDone } from '@/store/slices/todosSlice';
 
 import { TaskMenu } from '../TaskMenu';
 import { styles } from './styles';
 import { TaskContentProps } from './types';
 
-export const TaskContent = ({ title, description, isDone, onTaskPress }: TaskContentProps) => {
+export const TaskContent = ({ title, description, isDone, onTaskPress, id }: TaskContentProps) => {
+  const dispatch = useAppDispatch();
+
   const [isTaskMenuShown, setIsTaskMenuShown] = useState(false);
 
-  const onCheckBoxPress = () => {};
+  const onCheckBoxPress = () => {
+    dispatch(toggleTaskIsDone(id));
+  };
 
   const onTaskMenuToggleBtn = () => {
     setIsTaskMenuShown((prev) => !prev);
@@ -22,17 +28,23 @@ export const TaskContent = ({ title, description, isDone, onTaskPress }: TaskCon
   return (
     <Pressable onPress={onTaskPress} style={styles.task}>
       <View style={styles.timeRange}>
-        <Text variant={TextVariant.label_small}>10:00 am</Text>
-        <Text variant={TextVariant.label_small}>05:00 pm</Text>
+        <Text color={colors.primaryText} variant={TextVariant.label_small}>
+          10:00 am
+        </Text>
+        <Text color={colors.primaryText} variant={TextVariant.label_small}>
+          05:00 pm
+        </Text>
       </View>
       <Checkbox isDone={isDone} onPress={onCheckBoxPress} />
       <View style={styles.todoDescription}>
         <Text color={colors.primaryText} variant={TextVariant.body_large}>
           {title}
         </Text>
-        <Text variant={TextVariant.body_small}>{description}</Text>
+        <Text color={colors.primaryText} variant={TextVariant.body_small}>
+          {description}
+        </Text>
       </View>
-      {isTaskMenuShown && <TaskMenu />}
+      {isTaskMenuShown && <TaskMenu id={id} />}
       <Pressable onPress={onTaskMenuToggleBtn}>
         <Image style={styles.taskMenuToggleBtn} source={require('@/assets/icons/more.png')} />
       </Pressable>
