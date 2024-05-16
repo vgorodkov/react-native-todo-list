350b1ad03402975e1da82f8a45da245d2579b95a
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-16 10:20:28 +0300

    feat: add toggle checkbox functionality to subtasks

--- src/components/Subtask/index.tsx
+++ src/components/Subtask/index.tsx
@@ -1,5 +1,8 @@
 import { View } from 'react-native';
 
+import { colors } from '@/constants/colors';
+import { useAppDispatch } from '@/store/hooks';
+import { toggleSubtaskIsDone } from '@/store/slices/todosSlice';
 import { TodoSubtask } from '@/types/todo';
 
 import { Checkbox } from '../UI/CheckBox';
@@ -7,12 +10,21 @@ import { Text } from '../UI/Text';
 import { TextVariant } from '../UI/Text/types';
 import { styles } from './styles';
 
-export const Subtask = ({ title, isDone }: TodoSubtask) => {
-  const onCheckboxPress = () => {};
+interface TodoSubtaskProps extends TodoSubtask {
+  taskId: string;
+}
+
+export const Subtask = ({ title, isDone, id, taskId }: TodoSubtaskProps) => {
+  const dispatch = useAppDispatch();
+  const onCheckboxPress = () => {
+    dispatch(toggleSubtaskIsDone({ taskId, subTaskId: id }));
+  };
   return (
     <View style={styles.subTask}>
       <Checkbox isDone={isDone} onPress={onCheckboxPress} />
-      <Text variant={TextVariant.label_small}>{title}</Text>
+      <Text color={colors.primaryText} variant={TextVariant.label_small}>
+        {title}
+      </Text>
     </View>
   );
 };
