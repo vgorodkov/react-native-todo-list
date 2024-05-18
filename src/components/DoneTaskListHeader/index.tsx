c670b36964651f6ab5b2ebde54e80d43cf78741e
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-18 20:28:49 +0300

    chore: add task amount indicator

--- src/components/DoneTaskListHeader/index.tsx
+++ src/components/DoneTaskListHeader/index.tsx
@@ -3,10 +3,16 @@ import { Image, Pressable } from 'react-native';
 import { Text } from '../UI/Text';
 import { styles } from './styles';
 
-export const DoneTaskListHeader = ({ onPress }: { onPress: () => void }) => {
+export const DoneTaskListHeader = ({
+  onPress,
+  doneTasksAmount,
+}: {
+  onPress: () => void;
+  doneTasksAmount: number;
+}) => {
   return (
     <Pressable style={styles.doneTaskListHeader} onPress={onPress}>
-      <Text>Done tasks</Text>
+      <Text>done tasks ({doneTasksAmount})</Text>
       <Image style={[styles.arrowDownIcon]} source={require('@/assets/icons/arrow_down.png')} />
     </Pressable>
   );
