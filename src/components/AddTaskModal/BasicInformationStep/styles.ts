3b8902ad59affb6b745c1e2d3b178ffe313895c3
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-18 20:02:30 +0300

    feat: create first step BasicInformation for AddTaskModal

---
+++ src/components/AddTaskModal/BasicInformationStep/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+import { spacing } from '@/constants/spacing';
+
+export const styles = StyleSheet.create({
+  container: {
+    gap: spacing.m,
+  },
+});
