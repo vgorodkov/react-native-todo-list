be4c21ad37dd3637fffa0780f96cad83161bb3eb
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-18 20:19:38 +0300

    chore: add reexports

---
+++ src/components/AddTaskModal/index.ts
@@ -0,0 +1,5 @@
+export { BasicInformationStep } from './BasicInformationStep';
+export { ControlBtns } from './ControlBtns';
+export { DateStep } from './DateStep';
+export { SubtasksStep } from './SubtasksStep';
+export { TimerangeStep } from './TimerangeStep';
