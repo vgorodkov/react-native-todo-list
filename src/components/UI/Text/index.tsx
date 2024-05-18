97439f9644b3792aa8b56294f9fbfcaab7f8d179
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-18 20:35:05 +0300

    chore: add basic color

--- src/components/UI/Text/index.tsx
+++ src/components/UI/Text/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Text as RNText } from 'react-native';
 
+import { colors } from '@/constants/colors';
+
 import { textStyles } from './styles';
 import { TextProps } from './types';
 
 export const Text = ({ children, variant, style, color }: TextProps) => {
   const txtStyle = textStyles[variant ?? 'body_large'];
 
-  return <RNText style={[txtStyle, style, { color }]}>{children}</RNText>;
+  return (
+    <RNText style={[txtStyle, style, { color: color ?? colors.primaryText }]}>{children}</RNText>
+  );
 };
