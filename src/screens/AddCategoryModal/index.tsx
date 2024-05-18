a338cdb56410e3eda4d2952c60c5fa651352b60d
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-18 20:34:52 +0300

    refactor: move to UI Modal component

--- src/screens/AddCategoryModal/index.tsx
+++ src/screens/AddCategoryModal/index.tsx
@@ -3,8 +3,10 @@ import { View } from 'react-native';
 
 import { CategoryColor } from '@/components/CategoryColor';
 import { Button, Input, Text } from '@/components/UI';
+import { Modal } from '@/components/UI/Modal';
 import { TextVariant } from '@/components/UI/Text/types';
 import { colors } from '@/constants/colors';
+import { spacing } from '@/constants/spacing';
 import { useAppDispatch } from '@/store/hooks';
 import { addCategory } from '@/store/slices/categorySlice';
 
@@ -37,37 +39,35 @@ export const AddCategoryModal = ({ navigation }: AddCategoryModalScreenProps) =>
   };
 
   return (
-    <View style={styles.modalBackdrop}>
-      <View style={styles.modalContent}>
-        <Input
-          value={categoryTitle}
-          onChangeText={setCategoryTitle}
-          maxLength={CATEGORY_TITLE_LIMIT}
-          placeholder="Write category name"
-          label="Category name"
-        />
-        <Text color={colors.primaryText} variant={TextVariant.label_large}>
-          Avaible colors:
-        </Text>
-        <View style={styles.avaibleColorsContainer}>
-          {avaibleColors.map((color) => (
-            <CategoryColor
-              selectColor={setSelectedColor}
-              isSelected={color === selectedColor}
-              key={color}
-              color={color}
-            />
-          ))}
-        </View>
-        <View style={styles.footerBtnContainer}>
-          <Button variant="text" onPress={closeModal}>
-            Close
-          </Button>
-          <Button variant="text" disabled={!isInputValid} onPress={onAddCategoryBtnPress}>
-            Add
-          </Button>
-        </View>
+    <Modal style={{ margin: spacing.xl, padding: spacing.l, paddingBottom: spacing.s }}>
+      <Input
+        value={categoryTitle}
+        onChangeText={setCategoryTitle}
+        maxLength={CATEGORY_TITLE_LIMIT}
+        placeholder="Write category name"
+        label="Category name"
+      />
+      <Text color={colors.primaryText} variant={TextVariant.label_large}>
+        Avaible colors:
+      </Text>
+      <View style={styles.avaibleColorsContainer}>
+        {avaibleColors.map((color) => (
+          <CategoryColor
+            selectColor={setSelectedColor}
+            isSelected={color === selectedColor}
+            key={color}
+            color={color}
+          />
+        ))}
       </View>
-    </View>
+      <View style={styles.footerBtnContainer}>
+        <Button variant="text" onPress={closeModal}>
+          Close
+        </Button>
+        <Button variant="text" disabled={!isInputValid} onPress={onAddCategoryBtnPress}>
+          Add
+        </Button>
+      </View>
+    </Modal>
   );
 };
--- src/screens/AddCategoryModal/styles.ts
+++ src/screens/AddCategoryModal/styles.ts
@@ -3,21 +3,6 @@ import { StyleSheet } from 'react-native';
 import { spacing } from '@/constants/spacing';
 
 export const styles = StyleSheet.create({
-  modalBackdrop: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    backgroundColor: '#00000060',
-  },
-  modalContent: {
-    margin: spacing.xl,
-    paddingTop: spacing.l,
-    paddingBottom: spacing.s,
-    paddingHorizontal: spacing.xl,
-    gap: spacing.m,
-    borderRadius: spacing.s,
-    backgroundColor: 'white',
-  },
   avaibleColorsContainer: { flexDirection: 'row', gap: spacing.m, justifyContent: 'center' },
   footerBtnContainer: { justifyContent: 'flex-end', flexDirection: 'row', gap: spacing.m },
 });
