708d046d7a92c14a224176000b2efc387ae68f06
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-18 20:13:56 +0300

    feat: create Header which handle isImportant

---
+++ src/components/AddTaskModal/Header/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  empty: { width: 24 },
+  isImportantIcon: { width: 24, height: 24 },
+});
