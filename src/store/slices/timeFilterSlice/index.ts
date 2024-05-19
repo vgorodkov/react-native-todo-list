e3f0dee47a38d04ed62120b0225dc653a496fbf9
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-19 19:27:08 +0300

    chore: add time filter type

--- src/store/slices/timeFilterSlice/index.ts
+++ src/store/slices/timeFilterSlice/index.ts
@@ -1,7 +1,7 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 
-type TimeFilter = 'Today' | 'Week' | 'Month';
+import { TimeFilter } from '@/types/timefilter';
 
 interface TimeFilterState {
   selected: TimeFilter;
