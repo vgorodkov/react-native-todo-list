e4bd4e67f57fb5dafea837d2d58a90acb1b74244
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-19 16:34:25 +0300

    chore: use timeFilter logic from store

--- src/components/TimeFiltersRow/TimeFilter/index.tsx
+++ src/components/TimeFiltersRow/TimeFilter/index.tsx
@@ -1,15 +1,29 @@
-import { View } from 'react-native';
+import { Pressable } from 'react-native';
 
 import { Text } from '@/components/UI';
 import { TextVariant } from '@/components/UI/Text/types';
+import { colors } from '@/constants/colors';
+import { useAppDispatch } from '@/store/hooks';
+import { setTimeFilter } from '@/store/slices/timeFilterSlice';
 
 import { styles } from './styles';
 import { TimeFilterProps } from './types';
 
-export const TimeFilter = ({ title }: TimeFilterProps) => {
+export const TimeFilter = ({ title, isSelected }: TimeFilterProps) => {
+  const dispatch = useAppDispatch();
+
+  const onTimeFilterPress = () => {
+    dispatch(setTimeFilter(title));
+  };
+
   return (
-    <View style={styles.timeFilter}>
-      <Text variant={TextVariant.label_large}>{title}</Text>
-    </View>
+    <Pressable
+      onPress={onTimeFilterPress}
+      style={[styles.timeFilter, isSelected && styles.activeTimeFilter]}
+    >
+      <Text color={isSelected ? 'white' : colors.primaryText} variant={TextVariant.label_large}>
+        {title}
+      </Text>
+    </Pressable>
   );
 };
--- src/components/TimeFiltersRow/TimeFilter/types.ts
+++ src/components/TimeFiltersRow/TimeFilter/types.ts
@@ -1,3 +1,4 @@
 export interface TimeFilterProps {
   title: string;
+  isSelected: boolean;
 }
