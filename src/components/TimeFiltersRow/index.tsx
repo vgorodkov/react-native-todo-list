5b919adff2f536776998905d141119f54694105c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-19 16:34:03 +0300

    chore: use timeFilter from store

--- src/components/TimeFiltersRow/index.tsx
+++ src/components/TimeFiltersRow/index.tsx
@@ -1,14 +1,22 @@
 import { View } from 'react-native';
 
+import { useAppSelector } from '@/store/hooks';
+import { selectTimeFilter } from '@/store/slices/timeFilterSlice/selectors';
+
 import { timeFilters } from './constants';
 import { styles } from './styles';
 import { TimeFilter } from './TimeFilter';
 
 export const TimeFiltersRow = () => {
+  const selectedTimefilter = useAppSelector(selectTimeFilter);
   return (
     <View style={styles.timeFiltersContainer}>
       {timeFilters.map((timeFilter) => (
-        <TimeFilter key={timeFilter} title={timeFilter} />
+        <TimeFilter
+          isSelected={selectedTimefilter === timeFilter}
+          key={timeFilter}
+          title={timeFilter}
+        />
       ))}
     </View>
   );
