21158ed1223b653578ed995f9cf99eb41cab5930
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-20 14:48:00 +0300

    refactor: remove unused color

--- src/components/DoneTaskListHeader/styles.ts
+++ src/components/DoneTaskListHeader/styles.ts
@@ -1,11 +1,9 @@
 import { StyleSheet } from 'react-native';
 
-import { colors } from '@/constants/colors';
 import { spacing } from '@/constants/spacing';
 
 export const styles = StyleSheet.create({
   doneTaskListHeader: {
-    backgroundColor: colors.onPrimary,
     paddingTop: spacing.s,
     height: 40,
     borderTopWidth: 1,
