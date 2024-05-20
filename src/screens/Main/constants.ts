1a26f4d618a4f52caff30d7f1f951ba2d7dabf04
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-20 14:40:02 +0300

    feat: add gradient ellipses bg

---
+++ src/screens/Main/constants.ts
@@ -0,0 +1,16 @@
+export const leftEllipsisSize = { width: 280, height: 280 };
+export const leftGradientStops = [
+  { offset: '1%', color: '#646FD4', opacity: 1 },
+  { offset: '2%', color: '#646FD4', opacity: 1 },
+  { offset: '62%', color: '#646FD4', opacity: 1 },
+  { offset: '94%', color: '#99A0E3', opacity: 1 },
+  { offset: '100%', color: '#FFFFFF', opacity: 1 },
+];
+
+export const rightEllipsisSize = { width: 320, height: 310 };
+
+export const rightGradientStops = [
+  { offset: '0%', color: '#646FD4', opacity: 1 },
+  { offset: '55%', color: '#646FD4', opacity: 0.8 },
+  { offset: '100%', color: '#646FD4', opacity: 0 },
+];
