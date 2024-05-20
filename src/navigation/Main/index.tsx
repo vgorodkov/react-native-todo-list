ccfcabc0f455d20c32cf876415e65fefc2bc30d1
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-20 14:39:43 +0300

    chore: decompose screen options; add header title based on timefilter

--- src/navigation/Main/index.tsx
+++ src/navigation/Main/index.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { DrawerNavigation } from '@/navigation/Drawer';
 import { AddCategoryModal } from '@/screens/AddCategoryModal';
 import { ByTitleTasksScreen } from '@/screens/ByTitleTasks';
 import { CategoryTasksScreen } from '@/screens/CategoryTasks';
@@ -7,7 +8,7 @@ import { OnboardingScreen } from '@/screens/Onboarding';
 import { TaskModal } from '@/screens/TaskModal';
 import { MainStackParamList } from '@/types/navigation';
 
-import { DrawerNavigation } from './Drawer';
+import { HeaderTitleByParams } from './options';
 
 const Stack = createNativeStackNavigator<MainStackParamList>();
 
@@ -16,8 +17,10 @@ export const MainNavigation = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Onboarding" component={OnboardingScreen} />
       <Stack.Screen name="Drawer" component={DrawerNavigation} />
-      <Stack.Screen name="CategoryTasks" component={CategoryTasksScreen} />
-      <Stack.Screen name="ByTitleTasks" component={ByTitleTasksScreen} />
+      <Stack.Group screenOptions={HeaderTitleByParams}>
+        <Stack.Screen name="CategoryTasks" component={CategoryTasksScreen} />
+        <Stack.Screen name="ByTitleTasks" component={ByTitleTasksScreen} />
+      </Stack.Group>
       <Stack.Group screenOptions={{ presentation: 'transparentModal', animation: 'fade' }}>
         <Stack.Screen name="AddCategoryModal" component={AddCategoryModal} />
         <Stack.Screen name="TaskModal" component={TaskModal} />
