09cc7a0ce48ab6022f00dc60bfefb19b197fae28
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-20 14:47:49 +0300

    refactor: use const

--- src/components/TimeFiltersRow/styles.ts
+++ src/components/TimeFiltersRow/styles.ts
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 
+import { spacing } from '@/constants/spacing';
+
 export const styles = StyleSheet.create({
   timeFiltersContainer: {
+    marginTop: spacing.xl,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
   },
