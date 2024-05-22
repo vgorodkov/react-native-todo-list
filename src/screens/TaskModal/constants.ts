a0a5e1df137a160ba66be66982433311f34394d5
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-22 14:06:09 +0300

    refactor: remove unused code

--- src/screens/TaskModal/constants.ts
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from 'react-native';
-
-const { height, width } = Dimensions.get('window');
-
-export const MODAL_HEIGHT = height * 0.4;
-export const MODAL_WIDTH = width * 0.8;
--- src/utils/formatTime.ts
+++
@@ -1,3 +0,0 @@
-export const formatTime = (isoDate: string) => {
-  return isoDate.slice(11, 16);
-};
