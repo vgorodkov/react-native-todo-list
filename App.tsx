8d7df2428704b41207cfee901345f7001604682d
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-22 20:16:23 +0300

    refactor: move theme to constants

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
@@ -8,16 +8,9 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { colors } from '@/constants/colors';
+import { customTheme } from '@/constants/theme';
 import { MainNavigation } from '@/navigation/Main';
 import { persistor, store } from '@/store';
-
-const customTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: 'white',
-  },
-};
 
 const App = () => {
   useEffect(() => {
