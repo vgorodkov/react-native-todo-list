ea4547c147a8dfb453f7c189557dc4df4ee4260d
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-22 20:13:15 +0300

    chore: return -1 if the date is in the past

--- src/utils/getDaysFromNow.ts
+++ src/utils/getDaysFromNow.ts
@@ -1,6 +1,10 @@
 export const getDaysFromNow = (timestamp: number) => {
   const now = Date.now();
-  const diffInTime = Math.abs(now - timestamp);
+  const nowZero = new Date(now).setHours(0, 0, 0, 0);
+  const diffInTime = now - timestamp;
   const diffInDays = diffInTime / (1000 * 3600 * 24);
+  if (nowZero > timestamp) {
+    return -1;
+  }
   return diffInDays;
 };
