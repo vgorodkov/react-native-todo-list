a0e38203acd2fec11facf41aaed7c39025a75939
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-22 23:48:08 +0300

    fix: make diffInTime only positive to work correctly

--- src/utils/getDaysFromNow.ts
+++ src/utils/getDaysFromNow.ts
@@ -1,7 +1,7 @@
 export const getDaysFromNow = (timestamp: number) => {
   const now = Date.now();
   const nowZero = new Date(now).setHours(0, 0, 0, 0);
-  const diffInTime = now - timestamp;
+  const diffInTime = Math.abs(now - timestamp);
   const diffInDays = diffInTime / (1000 * 3600 * 24);
   if (nowZero > timestamp) {
     return -1;
