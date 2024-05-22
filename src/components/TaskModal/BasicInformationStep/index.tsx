6d96edf75aae95739fb0a3dc14ede80177d5f6ca
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-22 13:45:32 +0300

    refactor: rewrite BasicInformation logic and validation

--- src/components/TaskModal/BasicInformationStep/index.tsx
+++ src/components/TaskModal/BasicInformationStep/index.tsx
@@ -1,90 +1,44 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import React, { useEffect } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { Text, View } from 'react-native';
-import * as yup from 'yup';
+import { useFormContext, useWatch } from 'react-hook-form';
 
 import { ListItem } from '@/components/TaskModal/ListItem';
 import { Input } from '@/components/UI';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import { handleTextInfoInput, setNextStepIsAvaible } from '@/store/slices/taskModalSlice';
-import { selectDescription, selectTitle } from '@/store/slices/taskModalSlice/selectors';
 
+import { FormContext } from '../types';
 import { styles } from './styles';
 
-const schema = yup.object().shape({
-  title: yup.string().required('Title is required').min(3),
-  description: yup.string().required('Description is required').min(5),
-});
-
 export const BasicInformationStep = () => {
-  const dispatch = useAppDispatch();
-
-  const title = useAppSelector(selectTitle);
-  const description = useAppSelector(selectDescription);
-
   const {
-    control,
-    formState: { errors },
+    register,
     setValue,
-  } = useForm({
-    resolver: yupResolver(schema),
-    defaultValues: {
-      title,
-      description,
-    },
-  });
+    formState: { errors },
+    control,
+  } = useFormContext<FormContext>();
 
-  useEffect(() => {
-    if (!errors.title && !errors.description) {
-      dispatch(setNextStepIsAvaible(true));
-    } else {
-      dispatch(setNextStepIsAvaible(false));
-    }
-  }, [dispatch, errors.title, errors.description]);
+  const title = useWatch({ control, name: 'title' });
+  const description = useWatch({ control, name: 'description' });
 
-  const handleTitle = (text: string) => {
-    setValue('title', text, { shouldValidate: true });
-    dispatch(handleTextInfoInput({ mode: 'title', text }));
+  const handleTitleChange = (text: string) => {
+    setValue('title', text);
   };
-  const handleDescription = (text: string) => {
-    setValue('description', text, { shouldValidate: true });
-    dispatch(handleTextInfoInput({ mode: 'description', text }));
+  const handleDescriptionChange = (text: string) => {
+    setValue('description', text);
   };
 
   return (
     <ListItem style={styles.container}>
-      <Controller
-        control={control}
-        name="title"
-        render={({ field: { onBlur, value } }) => (
-          <View style={styles.inputContainer}>
-            <Input
-              value={value}
-              onChangeText={handleTitle}
-              onBlur={onBlur}
-              placeholder="Task title"
-            />
-            {errors.title && <Text style={styles.errorText}>{errors.title.message}</Text>}
-          </View>
-        )}
+      <Input
+        {...register('title')}
+        value={title}
+        onChangeText={handleTitleChange}
+        placeholder="Task title"
+        errorMessage={errors.title?.message}
       />
-      <Controller
-        control={control}
-        name="description"
-        render={({ field: { onBlur, value } }) => (
-          <View>
-            <Input
-              value={value}
-              onChangeText={handleDescription}
-              onBlur={onBlur}
-              placeholder="Task description"
-            />
-            {errors.description && (
-              <Text style={styles.errorText}>{errors.description.message}</Text>
-            )}
-          </View>
-        )}
+      <Input
+        {...register('description')}
+        value={description}
+        onChangeText={handleDescriptionChange}
+        placeholder="Task description"
+        errorMessage={errors.description?.message}
       />
     </ListItem>
   );
