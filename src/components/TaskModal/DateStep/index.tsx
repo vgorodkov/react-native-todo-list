e0c4720a7579df821b8291aec76f9df61bb5b1fa
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-22 13:47:47 +0300

    refactor: use common styles not to repeat styles

--- src/components/TaskModal/DateStep/index.tsx
+++ src/components/TaskModal/DateStep/index.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-native-date-picker';
 
+import { commonStyles } from '@/commonStyles';
 import { useAppDispatch } from '@/store/hooks';
 import { handleDateSelection } from '@/store/slices/taskModalSlice';
 
 import { ListItem } from '../ListItem';
-import { styles } from './styles';
 
 export const DateStep = () => {
   const dispatch = useAppDispatch();
+
   const [date, setDate] = useState(new Date());
 
   const onDateChange = (changedDate: Date) => {
     const timestamp = changedDate.getTime();
-    setDate(changedDate);
     dispatch(handleDateSelection(timestamp));
+    setDate(changedDate);
   };
 
   return (
-    <ListItem style={styles.container}>
+    <ListItem style={commonStyles.centeredView}>
       <DatePicker mode="date" date={date} onDateChange={onDateChange} />
     </ListItem>
   );
