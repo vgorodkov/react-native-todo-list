7e735e187967612ee2e77177f2b5ff40c3a6bea7
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-22 13:43:57 +0300

    chore: add common styles

--- src/commonStyles.ts
+++ src/commonStyles.ts
@@ -1,8 +1,23 @@
 import { StyleSheet } from 'react-native';
 
 export const commonStyles = StyleSheet.create({
+  fullSize: {
+    flex: 1,
+  },
   centeredView: {
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  fullSizeCentered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 4,
   },
 });
