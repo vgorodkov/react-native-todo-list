c489638f2167ba3a0731293c8be67c08562ca4ca
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-24 14:34:22 +0300

    refactor: use constants for routes

--- src/navigation/Main/types.ts
+++ src/navigation/Main/types.ts
@@ -1,7 +1,11 @@
 import { RouteProp } from '@react-navigation/native';
 
+import { RootStackRoutes } from '@/constants/navigation';
 import { RootStackParamList } from '@/types/navigation';
 
 export interface ScreensWithHeaderTitleProps {
-  route: RouteProp<RootStackParamList, 'ByTitleTasks' | 'CategoryTasks'>;
+  route: RouteProp<
+    RootStackParamList,
+    RootStackRoutes.BY_TITLE_TASKS | RootStackRoutes.CATEGORY_TASKS
+  >;
 }
